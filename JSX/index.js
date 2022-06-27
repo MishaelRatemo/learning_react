e9b45d2d035caa07ffa8e665f6526926b83cf135
MishaelRatemo <ratemomishael@gmail.com>
2022-06-27 12:58:41 +0300

    Solved a challenge on how to host 2 obbjects in a page usig ids

--- JSX/index.js
+++ JSX/index.js
@@ -1,6 +1,6 @@
 const page = (
     <nav>
-        <h1>Bob's Bistro</h1>
+        <h1>John's Shop</h1>
         <ul>
             <li>Menu</li>
             <li>About</li>
@@ -8,8 +8,43 @@ const page = (
         </ul>
     </nav>
 )
+
+// function Timer() {
+    // const element = (
+    //     <div>
+          
+    //       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    //     </div>
+    //   )
+      
+    // }
+    // setInterval(Timer, 1000);
+
+// // export default Timer
+// const timer = Timer()
+
+
 //render the content
+
+const test1 =(
+    <div>
+        <h1>Fun fatcs about React</h1>
+        <ul>
+            <li>Was released first in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars on github</li>
+            <li>is maintained by Facebool</li>
+            <li>Powers tousand of enterprice apps. including mobile apps</li>
+        </ul>
+    </div>
+)
+// Chhallenge one.
 ReactDOM.render(
-    page, 
+    // element,
+    page,
     document.getElementById("root")// the root is the id in the html file
     )
+ ReactDOM.render(
+    test1,
+    document.getElementById('test1')
+ )
