c9d8f987b6289fb950ed86b462266439dc9dec23
MishaelRatemo <ratemomishael@gmail.com>
2022-06-27 13:19:34 +0300

    composing components

--- components/index.js
+++ components/index.js
@@ -4,19 +4,21 @@
 not the functional way e.g <ComponentName /> not ComponentName().
 */
 // the element in the compont must bbe in one parent element
-
+function Header(){
+    <h1>Fun fatcs about React</h1>
+}
 function ComponentName() {
     return(
         <div>
-        <h1>Fun fatcs about React</h1>
-        <ul>
-            <li>Was released first in 2013</li>
-            <li>Was originally created by Jordan Walke</li>
-            <li>Has well over 100k stars on github</li>
-            <li>is maintained by Facebool</li>
-            <li>Powers tousand of enterprice apps. including mobile apps</li>
-        </ul>
-    </div>
+            <Header />
+            <ul>
+                <li>Was released first in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100k stars on github</li>
+                <li>is maintained by Facebool</li>
+                <li>Powers tousand of enterprice apps. including mobile apps</li>
+            </ul>
+         </div>
     )
 }
 
