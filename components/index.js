a9cdb83759b302481429d782409ffee3c22ddcc0
MishaelRatemo <ratemomishael@gmail.com>
2022-06-27 13:12:58 +0300

    created  react component

---
+++ components/index.js
@@ -0,0 +1,26 @@
+//// Conventions to follow when makeing a component
+// use pascal case not  to make functions
+/* call the component name using the the html element 
+not the functional way e.g <ComponentName /> not ComponentName().
+*/
+// the element in the compont must bbe in one parent element
+
+function ComponentName() {
+    return(
+        <div>
+        <h1>Fun fatcs about React</h1>
+        <ul>
+            <li>Was released first in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100k stars on github</li>
+            <li>is maintained by Facebool</li>
+            <li>Powers tousand of enterprice apps. including mobile apps</li>
+        </ul>
+    </div>
+    )
+}
+
+ReactDOM.render(
+    <ComponentName />,
+    document.getElementById('root')
+)
