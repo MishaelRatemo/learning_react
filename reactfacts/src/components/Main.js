4010b78674a3e2e1acd06f019a06053e6fb546a1
MishaelRatemo <ratemomishael@gmail.com>
2022-06-28 14:37:18 +0300

    added react background react

--- reactfacts/src/components/Main.js
+++ reactfacts/src/components/Main.js
@@ -1,8 +1,10 @@
 import React from 'react'
-
+// import bg from '../images/logo.svg'
 function Main() {
   return (
       <main>
+        {/* <img src={bg} className="nav--icon" alt='logo' /> */}
+        
           <h1 className="main--title">Fun facts about React</h1>
           <ul className="main--facts">
               <li>Was first released in 2013</li>
