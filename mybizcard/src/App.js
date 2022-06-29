e25e8583c5917af96742453c110214d6f7d39a97
MishaelRatemo <ratemomishael@gmail.com>
2022-06-29 11:32:29 +0300

    Added about component

--- mybizcard/src/App.js
+++ mybizcard/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import About from './components/About';
 import Info from './components/Info';
 function App() {
   return (
     <div className="App">
       <Info />
+      <About />
     </div>
   );
 }
