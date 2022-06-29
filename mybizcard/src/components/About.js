5f840c226e81ea0092876525263048382309cc1a
MishaelRatemo <ratemomishael@gmail.com>
2022-06-29 11:30:32 +0300

    Added structure to the component

---
+++ mybizcard/src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './About.css'
+function About() {
+    return (
+        <div className='about'>
+            <h1>About</h1>
+            <p>
+                I am a FullStack developer with a particular
+                interest in making things simple and
+                automating daily tasks. I try to keep
+                up with security and best practices,
+                and am always looking for new things
+                to learn.
+            </p>
+        </div>
+    )
+}
+
+export default About
