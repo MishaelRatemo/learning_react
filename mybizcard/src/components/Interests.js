7003275f187ec25ef57c5289b5a0408c21aadd36
MishaelRatemo <ratemomishael@gmail.com>
2022-06-29 11:37:34 +0300

    Created component structure

---
+++ mybizcard/src/components/Interests.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Interests() {
+    return (
+        <div className='interest'>
+            <h1>Interests</h1>
+            <p>
+                Technology. Science . Religion. 
+                Travel. Sci- Fi Movies. Entrepreneur. 
+                Reader. Mentoring. Food -vegans.
+            </p>
+        </div>
+    )
+}
+
+export default Interests
