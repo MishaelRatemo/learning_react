600d66a8d5fa776443675f62448f51dcfb1f8151
MishaelRatemo <ratemomishael@gmail.com>
2022-06-29 11:00:12 +0300

    Added classnames fro styling

--- mybizcard/src/components/Info.js
+++ mybizcard/src/components/Info.js
@@ -7,11 +7,12 @@ function Info() {
         <div className='info'>
             <img className='myimage' src={myimage} alt="myimage" />
             <div className="lower">
-                <h1>Mishael Ratemo</h1>
-                <p>FullStack Developer</p>
+                <h1 className='name'>Mishael Ratemo</h1>
+                <p className='stack'>FullStack Developer</p>
+                <small>ratemishael@gmail.com</small>
                 <div className="buttons">
-                    <input type="button" value="Email" />
-                    <input type="button" value="LinkedIn" />
+                    <button className='btn mail' type="button" value="Email" > Email</button>
+                    <button className='btn linkedin' type="button" value="LinkedIn" >Linkedin</button>
                 </div>
             </div>
         </div>
