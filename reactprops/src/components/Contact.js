59471b80e311db3020aaf9303b4aa9b5b0f778eb
MishaelRatemo <ratemomishael@gmail.com>
2022-07-19 11:54:43 +0300

    Added content of the conatcts and its props skeleton

--- reactprops/src/components/Contact.js
+++ reactprops/src/components/Contact.js
@@ -1,9 +1,23 @@
 import React from 'react'
+// import avt1 from '../images/avt1.png'
 
-function Contact() {
-  return (
-    <div>Contact</div>
-  )
+function Contact(props) {
+    return (
+        <div className='contacts'>
+            <div className="card">
+                <img src={props.img} alt="" />
+                <h3>{props.name}</h3>
+                <div className="info-group">
+                    <h2>&#9742;</h2>
+                    <p>{props.phone}</p>
+                </div>
+                <div className="info-group">
+                    <h2>&#128231;</h2>
+                    <p>{props.email}</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Contact
