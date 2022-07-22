8d30db0e2ab5069795a98210c6344cc98a7f1376
MishaelRatemo <ratemomishael@gmail.com>
2022-07-22 11:40:24 +0300

    Added data to data.js objects

---
+++ props-array-mapping/src/data.js
@@ -0,0 +1,44 @@
+ const data=[
+    {
+        pk:1,
+        title: 'Nairobi arboretum',
+        country: 'Kenya',
+        location:'Nairobi',
+        startDate: '11 Jun, 2022',
+        endDate:'11 Jun, 2022',
+        description:" This place is nice for family  and groups outing for chiling.",
+        image:'aboretum.jpg',
+    },
+    {
+        pk:2,
+        title: 'Karura Forest',
+        country: 'Kenya',
+        location:'Nairobi',
+        startDate: '11 May, 2023',
+        endDate:'11 May, 2022',
+        description:"This a natural forest placed in the outskirt of Nairobi city. This place is nice for family  and groups outing for chiling.",
+        image:'./images/aboretum.jpg',
+    },
+    {
+        pk:3,
+        title: 'Lake Elementaita',
+        country: 'Kenya',
+        location:'Nakuru',
+        startDate: '11 May, 2023',
+        endDate:'11 May, 2022',
+        description:"This a natural forest placed in the outskirt of Nairobi city. This place is nice for family  and groups outing for chiling.",
+        image:'./images/aboretum.jpg',
+    },
+    {
+        pk:4,
+        title: 'Oldonyo Sabuk ',
+        country: 'Kenya',
+        location:'Machakos',
+        startDate: '11 May, 2023',
+        endDate:'11 May, 2022',
+        description:"This a natural forest placed in the outskirt of Nairobi city. This place is nice for family  and groups outing for chiling.",
+        image:'./images/aboretum.jpg',
+    }
+]
+
+export default data;
