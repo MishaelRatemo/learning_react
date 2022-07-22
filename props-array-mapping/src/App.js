5235d07d19fb6f5ecfadccfec6a244a0235c11d3
MishaelRatemo <ratemomishael@gmail.com>
2022-07-22 11:40:45 +0300

    Added the card to App js

--- props-array-mapping/src/App.js
+++ props-array-mapping/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Card from './components/Card';
 import Nav from './components/Nav';
-
+import data from './data';
 function App() {
+  const cards_content = data.map((item) => {
+    return(
+      <Card 
+          key = {item.pk}
+          {...item}
+      />      
+    )    
+  })
+  
+
   return (
     <div className="App">
-      <Nav />      
+      <Nav />   
+      <section className='cards'>
+        {cards_content}
+      </section>   
     </div>
   );
 }
