a45502f8d2e4ec06c70bcf4b6edaac0f4c8bcd48
MishaelRatemo <ratemomishael@gmail.com>
2022-07-25 14:50:06 +0300

    Added class names for styling

--- props-array-mapping/src/components/Card.js
+++ props-array-mapping/src/components/Card.js
@@ -1,15 +1,20 @@
 import React from 'react'
 function Card(props) {
+  let locator = 'location-icon.png'
+
   return (
     <div className='card'>
         <img src={ `../images/${props.image}`}  className='locator' alt="" />
         <div className="cardcontent">
             <section>
-                <img src="" className='' alt="" />
-                <p>{props.location}, {props.country}</p>
+              <div className="card--upper">
+                <img src={`../images/${locator}`} className='locator' alt="" />
+                <p className='location'>{props.location}, {props.country} &nbsp;</p>
+                <p className='map'>Click here to view on Google  Maps</p>
+              </div>
                 <link className='view_map' rel="stylesheet" href="" />
             </section>
-            <h1 className='place'>{props.title}</h1>
+            <h1 className='placename'>{props.title}</h1>
             <h4 className='dates'>{props.startDate}- {props.endDate}</h4>
             <p className='description'>{props.description}</p>
         </div>        
