9945e2913d5e1a5d1065c7b3a57f1571cc92732e
MishaelRatemo <ratemomishael@gmail.com>
2022-07-27 12:55:16 +0300

    Created header> nav component

---
+++ reactstates/src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <header>
+      <nav>
+        <div>
+          <h2>meme Generator</h2>
+        </div>
+        <div>
+          <h3>React useState</h3>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
